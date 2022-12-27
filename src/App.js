84ffdef629b25fa3e9bb261a529459859155d09b
rdtungul <rosephdarl@gmail.com>
2022-12-27 19:02:10 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,93 +1,20 @@
-import "./App.css";
-import { HStack, VStack } from "@chakra-ui/react";
-import { MyProvider, useSettings } from "./ThemeContext";
-import Toggle from "./Switch";
-
-const Title = ({ children }) => {
-  const { settings } = useSettings();
-  return (
-    <h2
-      style={{
-        color: settings === "sun" ? "black" : "white",
-      }}
-    >
-      {children}
-    </h2>
-  );
-};
-
-const MyText = ({ children }) => {
-  const { settings } = useSettings();
-  return (
-    <p
-      style={{
-        color: settings === "sun" ? "black" : "white",
-      }}
-    >
-      {children}
-    </p>
-  );
-};
-
-const Content = () => {
-  return (
-    <div>
-      <VStack pt={15}>
-        <HStack alignItems="center">
-          <MyText>
-            I work as a part-time instructor and a freelance web developer in
-            the Philippines. I create unique websites and apps that you won't
-            find in a template.
-            <br />I have worked on web based projects for a range of clients
-            providing Web Development (PHP, MySQL, HTML5, JavaScript/ES6,
-            CSS/SASS, React, WordPress, Bootstrap, and more) and Design (Adobe
-            XD, Illustrator, Photoshop, InDesign, etc.).
-          </MyText>
-        </HStack>
-      </VStack>
-    </div>
-  );
-};
-
-const Head = () => {
-  return (
-    <header>
-      <Title>Roseph Darl 💎</Title>
-      <Toggle />
-    </header>
-  );
-};
-
-const Feed = () => {
-  return (
-    <div className="Feed">
-      <Title>Front-End Developer</Title>
-      <Content />
-    </div>
-  );
-};
+import "./App";
+import RegisterImg from "./formik/assets/register.jpg";
+import { SignUp } from "./formik/components/SignUp";
 
 function App() {
-  const { settings } = useSettings();
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor: settings === "sun" ? "white" : "black",
-      }}
-    >
-      <Head />
-      <Feed />
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-5">
+          <SignUp />
+        </div>
+        <div className="col-md-7 my-auto">
+          <img src={RegisterImg} alt="HeroImg" className="img-fluid w-50" />
+        </div>
+      </div>
     </div>
   );
 }
 
-function Root() {
-  return (
-    <MyProvider>
-      <App />
-    </MyProvider>
-  );
-}
-
-export default Root;
+export default App;
